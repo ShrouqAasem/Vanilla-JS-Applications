54c5f6b1317fc3ab92a5d7dd3c22fddff96fa9bc
Shrouq Aasem <Eng.Shrouq.Aasem@gmail.com>
2023-05-25 20:37:15 +0300

    initialize Quiz-App

---
+++ Quiz-App/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'what is the most used programming language in 2019?',
+        a: 'java',
+        b: 'c',
+        c: 'python',
+        d: 'javascript',
+        correct: 'd'
+    }, {
+        question: 'who is the president of us?',
+        a: 'florin pop',
+        b: 'DonaldTrump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    }, {
+        question: 'what does HTML stands for?',
+        a: 'hyper text markup language',
+        b: 'cascading style sheet',
+        c: 'json object notation',
+        d: 'application programming interface',
+        correct: 'a'
+    }, {
+        question: 'what year was javascript launched?',
+        a: '2020',
+        b: '2019',
+        c: '2018',
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
