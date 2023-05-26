c7b26ab66259699cf2b4741d2e93c88366a90266
Shrouq Aasem <Eng.Shrouq.Aasem@gmail.com>
2023-05-26 03:42:48 +0300

    complete version

--- Quiz-App/README.md
+++ Quiz-App/README.md
@@ -1,3 +1,3 @@
-<h1>Project Title</h1>
-<h2>Sub Title</h2>
-<p>Description</p>
+<h1>Quiz-App</h1>
+
+<p>"This is a simple Quiz App with a user-friendly interface. The app generates multiple-choice questions (MCQs) one by one and then displays the score of your correct answers after submitting all the questions." </p>
--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -1,38 +1,100 @@
 const quizData = [
     {
-        question: 'How old is florin?',
-        a: '10',
-        b: '17',
-        c: '26',
-        d: '110',
-        correct: 'c'
-    }, {
-        question: 'what is the most used programming language in 2019?',
-        a: 'java',
-        b: 'c',
-        c: 'python',
-        d: 'javascript',
-        correct: 'd'
-    }, {
-        question: 'who is the president of us?',
-        a: 'florin pop',
-        b: 'DonaldTrump',
-        c: 'Ivan Saldano',
-        d: 'Mihai Andrei',
+        question: 'What is the capital of Spain?',
+        a: 'Lisbon',
+        b: 'Madrid',
+        c: 'Paris',
+        d: 'Rome',
         correct: 'b'
-    }, {
-        question: 'what does HTML stands for?',
-        a: 'hyper text markup language',
-        b: 'cascading style sheet',
-        c: 'json object notation',
-        d: 'application programming interface',
-        correct: 'a'
-    }, {
-        question: 'what year was javascript launched?',
-        a: '2020',
-        b: '2019',
-        c: '2018',
-        d: 'none of the above',
-        correct: 'd'
+    }, 
+    {
+        question: 'Which ocean is the largest by area?',
+        a: 'Indian Ocean',
+        b: 'Pacific Ocean',
+        c: 'Atlantic Ocean',
+        d: 'Southern Ocean',
+        correct: 'b'
+    }, 
+    {
+        question: 'When did the French Revolution begin?',
+        a: '1776',
+        b: '1789',
+        c: '1812',
+        d: '1848',
+        correct: 'b'
+    }, 
+    {
+        question: 'Which country has won the most FIFA World Cup titles?',
+        a: 'Italy',
+        b: 'Germany',
+        c: 'Brazil',
+        d: 'Argentina',
+        correct: 'c'
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz= 0;
+let score = 0;
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b; 
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+    
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You got ${score}/${quizData.length} questions.</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
     }
-]
+});
